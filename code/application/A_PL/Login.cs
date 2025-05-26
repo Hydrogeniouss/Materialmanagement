342e1e15a506d1b27b1cffcb545d90404c59a198
Gabelfisch <gabrielsinz@gmx.at>
2025-05-26 09:01:21 +0200

    implemented login check

--- code/application/A_PL/Login.cs
+++ code/application/A_PL/Login.cs
@@ -16,5 +16,31 @@ namespace application
             var materials = Member.FromDatabase();
             MessageBox.Show($"There are {materials.Count} materials in the database.");
         }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Member mem = Member.FromDatabase().Where(mem => mem.FirstName == tbx_firstName.Text
+                    && mem.LastName == tbx_lastName.Text).ToArray()[0];
+
+                if (mem.Pin.ToString() == tbx_pin.Text.Trim()) 
+                {
+                    
+
+                }
+            }
+            catch
+            {
+                lbl_errorMessage.Text = "Der Vor- oder Nachname oder der PIN ist Falsch.";
+            }
+
+            
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
